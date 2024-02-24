803fa48f467d60b7c074a9fd3eb62cc9cdc92173
K1-aD <153174317+K1-aD@users.noreply.github.com>
2024-02-24 08:53:56 +0800

    Edited files

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,7 +4,12 @@ import {Box, Button, Stack, TextField, Typography} from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <stack alignItems="center" mt="37px" justifyContent="center" p="20px">
+        <Typography sx={{
+        fontSize: {lg:'45px', xs:'34px'}}}mb="50px" textAlign="Center"> 
+                Most Popular Workouts and Diets
+        </Typography>
+    </stack>
   )
 }
 
