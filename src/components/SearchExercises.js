f9dc25bc82b7a08e0337b607c928800f2d9c6f14
K1-aD <153174317+K1-aD@users.noreply.github.com>
2024-02-24 08:04:19 +0800

    Add files via upload

---
+++ src/components/SearchExercises.js
@@ -0,0 +1,11 @@
+import React, {useEffect,useState} from 'react';
+import {Box, Button, Stack, TextField, Typography} from '@mui/material';
+
+
+const SearchExercises = () => {
+  return (
+    <div>SearchExercises</div>
+  )
+}
+
+export default SearchExercises
